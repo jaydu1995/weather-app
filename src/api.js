2d2d0d3b8898ebe57b015105d13d11a7d52a32ff
Jeremiah Duhart <jaydu1995@gmail.com>
2021-01-15 21:55:40 -0500

    Fixed mixed content error.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export async function getWeatherData(city = "Miami") {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=44fa4677d06f6c3ead606767129be260&units=imperial`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=44fa4677d06f6c3ead606767129be260&units=imperial`,
     { mode: "cors" }
   );
 
